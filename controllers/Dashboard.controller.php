c58eddd92b43d7b0590f52d30e37e6330ae68bb4
THJOP <antonio.fernandes@rbsolucoes.com.br>
2019-08-14 17:36:56 -0300

    teste

---
+++ controllers/Dashboard.controller.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Controlador que deverá ser chamado quando não for
+ * especificado nenhum outro
+ *
+ * Camada - Controladores ou Controllers
+ *
+ * @package Sistema distribuido em modulos
+ * @author AVF-WEB
+ * @version 1.0Locacao
+ * */
+require_once ABSPATH . "/models/class/Dashboard.model.php";
+
+class DashboardController extends MainController {
+
+    /**
+     * IndexController constructor.
+     * Define qual rota seguir
+     */
+    public function __construct(){
+        $this->checkLogado();
+    }
+
+    /**
+     * Ação que deverá ser executada quando
+     * nenhuma outra for especificada, do mesmo jeito que o
+     * arquivo index.html ou index.php é executado quando nenhum
+     * é referenciado
+     */
+    public function indexAction() {
+        echo 'Dashboard';
+    }
+
+
+}
