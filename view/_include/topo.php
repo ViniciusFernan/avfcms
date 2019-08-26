3deee52ead94c8899b1248687bd3c3fa9ab8ecfe
THJOP <antonio.fernandes@rbsolucoes.com.br>
2019-08-26 18:07:01 -0300

    denovo

--- view/_include/topo.php
+++ view/_include/topo.php
@@ -6,4 +6,21 @@
             <a class="btn btn-sm btn-outline-secondary" href="<?= HOME_URI ?>/login/logout">SAIR</a>
         </li>
     </ul>
+
+    <ul  class="navbar-nav navbar-right nav">
+        <li class="dropdown">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">EN <span class="caret"></span></a>
+            <ul id="w2" class="dropdown-menu">
+                <li>
+                    <a href="https://hotexamples.com/examples/-/PhpOffice%255CPhpWord%255CTemplateProcessor/-/php-phpoffice%255cphpword%255ctemplateprocessor-class-examples.html" tabindex="-1">EN</a>
+                </li>
+                <li>
+                    <a href="https://hotexamples.com/ru/examples/-/PhpOffice%255CPhpWord%255CTemplateProcessor/-/php-phpoffice%255cphpword%255ctemplateprocessor-class-examples.html" tabindex="-1">RU</a>
+                </li>
+                <li>
+                    <a href="https://hotexamples.com/de/examples/-/PhpOffice%255CPhpWord%255CTemplateProcessor/-/php-phpoffice%255cphpword%255ctemplateprocessor-class-examples.html" tabindex="-1">DE</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
 </div>
