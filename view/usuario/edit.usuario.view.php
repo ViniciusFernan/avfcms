903a3ccba83be8e489391fb4c3b7d8ae3a9844be
THJOP <antonio.fernandes@rbsolucoes.com.br>
2019-08-29 09:43:35 -0300

    denovo

--- view/usuario/edit.usuario.view.php
+++ view/usuario/edit.usuario.view.php
@@ -48,34 +48,92 @@ $logoSistema = THEME_URI . "/_assets/images/LOGO_DEFAULT.png";
                         <div class="card-title">Editar Usuário</div>
                     </div>
                     <div class="card-body">
+
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="email">Nome:</label>
-                                    <input type="nome" class="form-control" placeholder="Nome">
-                                </div>
-                            </div>
-
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="email">Sobre Nome:</label>
-                                    <input type="sobreNome" class="form-control" placeholder="Sobre Nome">
+                                    <label for="imgPerfil">Imagem de perfil:</label>
+                                    <input type="imgPerfil" class="form-control" placeholder="imgPerfil">
                                 </div>
                             </div>
                         </div>
 
                         <div class="row">
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <div class="form-group">
-                                    <label for="email">Email:</label>
+                                    <label for="nome">Nome:</label>
                                     <input type="nome" class="form-control" placeholder="Nome">
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <div class="form-group">
-                                    <label for="email">Sobre Nome:</label>
+                                    <label for="sobreNome">Sobre Nome:</label>
                                     <input type="sobreNome" class="form-control" placeholder="Sobre Nome">
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="cpf">CPF:</label>
+                                    <input type="cpf" class="form-control" placeholder="CPF">
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="email">Email:</label>
+                                    <input type="email" class="form-control" placeholder="Email">
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="telefone">Telefone:</label>
+                                    <input type="telefone" class="form-control" placeholder="Telefone">
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="senha">Senha de acesso:</label>
+                                    <input type="senha" class="form-control" placeholder="Senha de acesso">
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="dataNascimento">Data de nascimento:</label>
+                                    <input type="dataNascimento" class="form-control" placeholder="Data de nascimento">
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="sexo">Sexo:</label>
+                                    <input type="sexo" class="form-control" placeholder="Sexo">
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="status">Status:</label>
+                                    <input type="status" class="form-control" placeholder="Status">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="detalhes">Detalhes:</label>
+                                    <input type="detalhes" class="form-control" placeholder="Detalhes">
                                 </div>
                             </div>
                         </div>
