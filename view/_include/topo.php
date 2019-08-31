8753a7bbbff207f5b64c5bb90591650a36fa3a38
vinicius <viniciusblink@hotmail.com>
2019-08-31 13:46:54 -0300

    teste e

--- view/_include/topo.php
+++ view/_include/topo.php
@@ -2,8 +2,8 @@
 ?>
 <div class="main-header">
     <div class="logo-header">
-        <a href="index.html" class="logo">
-            Ready Dashboard
+        <a href="<?=HOME_URI?>/" class="logo">
+            AVF - CMS
         </a>
         <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -17,9 +17,9 @@
                 <div class="input-group">
                     <input type="text" placeholder="Search ..." class="form-control">
                     <div class="input-group-append">
-								<span class="input-group-text">
-									<i class="la la-search search-icon"></i>
-								</span>
+                        <span class="input-group-text">
+                            <i class="la la-search search-icon"></i>
+                        </span>
                     </div>
                 </div>
             </form>
