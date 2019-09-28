32e34975b7dbd8fbc45d1b0e0fdceeaa2387fc7e
vinicius <viniciusblink@hotmail.com>
2019-09-27 21:12:18 -0300

    create controller anuncio

---
+++ controllers/AnuncioController.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Controlador que deverá ser chamado quando não for
+ * especificado nenhum outro
+ *
+ * Camada - Controladores ou Controllers
+ *
+ * @package Sistema distribuido em modulos
+ * @author AVF-WEB
+ * @version 1.0Locacao
+ * */
+require_once ABSPATH . "/models/class/AnuncioModel.php";
+
+class AnuncioController extends MainController {
+
+    /**
+     * IndexController constructor.
+     * Define qual rota seguir
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Ação que deverá ser executada quando
+     * nenhuma outra for especificada, do mesmo jeito que o
+     * arquivo index.html ou index.php é executado quando nenhum
+     * é referenciado
+     */
+    public function indexAction() {
+        echo 'Dashboard';
+    }
+
+
+}
