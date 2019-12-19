fa911daed00c6255005795759a21f1a82a8746d5
Vinicius Fernandes <antonio.fernandes@rbsolucoes.com.br>
2019-12-19 14:01:20 -0300

    teste rename

--- controllers/CadastroController.php
+++ controllers/CadastroController.php
@@ -26,24 +26,25 @@ class CadastroController extends MainController {
     }
 
     public function cadastrarNovoUsuarioAction(){
-        $error = false;
-
-        $usuario = new AnuncioModel;
-
-        $insertResp = $usuario->novoUsuario($this->parametrosPost);
-        if(empty($insertResp) && !is_int($insertResp) ) {
-            unset($this->parametrosPost["senha"]);
-            $this->retorno=['msg' => "Erro ao cadastrar usuário!", 'tipo' => 'danger'];
-            $error = true;
-        }else if(!empty($insertResp) && !is_int($insertResp) ){
-            $this->retorno=['msg' => $insertResp, 'tipo' => 'danger'];
-            $error = true;
-        }else{
-            $this->retorno['resp'] = $insertResp;
-            $this->retorno=['msg'=>"Usuário cadastrado com sucesso!", 'tipo'=>'success'];
+        try{
             $error = false;
-        }
 
+            $insertResp = (new UsuarioModel())->novoUsuario($this->parametrosPost);
+            if(empty($insertResp) && !is_int($insertResp) ) {
+                unset($this->parametrosPost["senha"]);
+                $this->retorno=['msg' => "Erro ao cadastrar usuário!", 'tipo' => 'danger'];
+                $error = true;
+            }else if(!empty($insertResp) && !is_int($insertResp) ){
+                $this->retorno=['msg' => $insertResp, 'tipo' => 'danger'];
+                $error = true;
+            }else{
+                $this->retorno['resp'] = $insertResp;
+                $this->retorno=['msg'=>"Usuário cadastrado com sucesso!", 'tipo'=>'success'];
+                $error = false;
+            }
+        }catch (Exception $exception){
+            return $exception;
+        }
 
         $View = new View('cadastro/cadastro.view.php');
         $View->addParams('boxMsg', $this->retorno);
@@ -54,27 +55,26 @@ class CadastroController extends MainController {
 
     /***********************************   AJAX   ********************************************/
     public function checarSeUsuariosJaExisteAjaxAction(){
-        $usuario = new AnuncioModel;
+        try{
+            $usuario = new ChecaCadastroUsuarioStrategy();
 
-        if(!empty($this->parametrosPost['email'])) {
-            $selectResp = $usuario->checarEmailJaEstaCadastrado(['email' => $this->parametrosPost['email']] );
-            if(!empty($selectResp)){
-                $this->retorno['resp'] = false;
-                $this->retorno['boxMsg'] = ['msg' => "Este email já esta cadastrado!", 'tipo' => 'danger'];
+            if(!empty($this->parametrosPost['email'])) {
+                $selectResp = $usuario->checaCadastradoUsuario(['email' => $this->parametrosPost['email']] );
+                if(!empty($selectResp)){
+                    echo json_encode( [ "status" => 'error', "msg" => "Este email já esta cadastrado!", 'tipo' => 'danger' ] );
+                }
             }
+
+            if(!empty($this->parametrosPost['CPF']) && $selectResp==false) {
+                $selectResp = $usuario->checaCadastradoUsuario(['CPF' => $this->parametrosPost['CPF']] );
+                if(!empty($selectResp)){
+                    echo json_encode( [ "status" => 'error', "msg" => "Este CPF já esta cadastrado!", 'tipo' => 'danger' ] );
+                }
+            }
+        }catch (Exception $exception){
+            echo json_encode( [ "status" => 'error', "msg" => $exception->getMessage() ] );
         }
 
-        if(!empty($this->parametrosPost['CPF']) && $selectResp==false) {
-            $selectResp = $usuario->checarCPFJaEstaCadastrado(['CPF' => $this->parametrosPost['CPF']] );
-            if(!empty($selectResp)){
-                $this->retorno['resp'] = false;
-                $this->retorno['boxMsg'] = ['msg' => "Este CPF já esta cadastrado!", 'tipo' => 'danger'];
-            }
-        }
-
-        $View = new View('cadastro/cadastro.view.php');
-        $View->setParams($this->retorno);
-        $View->showContents();
     }
 
 }
