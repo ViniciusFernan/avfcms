33454efbf3e9d95de55eaeea139b67250bc054e3
Vinicius <antonio.fernandes@rbsolucoes.com.br>
2019-12-19 18:26:03 -0300

    calculos

--- core/Conn/Select.DAO.class.php
+++ core/Conn/Select.DAO.class.php
@@ -5,8 +5,20 @@
  */
 class Select extends Conn
 {
+    //$where[]['data'] = ['comparison' => '=', 'field' => 'cpp.id_comissao_data_corte', 'value' => $dsParam['idComissaoDataCorte']];
 
-    private $Select;
+    //$join[] = [
+    //'tipoRelacao' => 'JOIN',
+    //'tabela' => 'pedidos_compra_beneficios',
+    //'data' => [
+    //['comparison' => '=', 'field' => 'pedido_compra_pendencia_2_via.id_pedido_compra_beneficio', 'value' => 'pedidos_compra_beneficios.id']
+    //]
+    //];
+
+
+    private $Select = 'SELECT ';
+    private $SqlFoundRows = 'SQL_CALC_FOUND_ROWS';
+
     /** @var PDO */
     private $Conn;
     /** @var PDOStatement */
@@ -57,6 +69,70 @@ class Select extends Conn
     private function setJoin($join)
     {
         try {
+            if(!empty($join) && !is_array($join)) throw new Exception('Erro em dado enviado ');
+
+            $partQuery = '';
+
+            foreach ($join as $key => $item)
+                {
+                    $partQuery .= ' ' . $item['relacao'];
+                    $partQuery .= ' ' . $item['tabela'];
+
+                    if (isset($item['data']))
+                    {
+                        $countFiltrosAux = 0;
+                        foreach ($item['data'] as $_key => $_dataItem)
+                        {
+                            if ( isset($_array['in'])  || isset($_array['notin']) || isset($_array['like'])) throw new Exception('Sem valores para realizar a pesquisa do JOIN.');
+
+                            $countFiltrosAux++;
+                            $query .= ($countFiltrosAux == 1 ? ' ON' : '');
+
+                            $operator = '=';
+
+                            if (isset($_array['comparison']))
+                            {
+                                $operator = $_array['comparison'];
+                            }
+
+                            if (isset($_array['in']))
+                            {
+                                $query .= ($countFiltrosAux > 1 ? ' AND ' : ' ') . $_array['field'] . ' IN (' . $_array['value'] . ')';
+                            }
+                            else if (isset($_array['notin']))
+                            {
+                                $query .= ($countFiltrosAux > 1 ? ' AND ' : ' ') . $_array['field'] . ' NOT IN (' . $_array['value'] . ')';
+                            }
+                            else if (isset($_array['like']))
+                            {
+                                if (isset($_array['like']['order']))
+                                {
+                                    $porcentagemComeco = ($_array['like']['order'] == 'front' ? '%' : '');
+                                    $porcentagemFim = ($_array['like']['order'] == 'back' ? '%' : '');
+                                    $query .= ($countFiltrosAux > 1 ? ' AND ' : ' ') . $_array['field'] . ' LIKE \'' . $porcentagemComeco . addslashes($_array['value']) . $porcentagemFim . '\'';
+                                }
+                                else
+                                {
+                                    $query .= ($countFiltrosAux > 1 ? ' AND ' : ' ') . $_array['field'] . ' LIKE \'%' . addslashes($_array['value']) . '%\'';
+                                }
+                            }
+                            else if ($operator == 'IS' || $operator == 'IS NOT')
+                            {
+                                $query .= ($countFiltrosAux > 1 ? ' AND ' : ' ') . $_array['field'] . ' ' . $operator . ' ' . $_array['value'];
+                            }
+                            else if ($operator == 'OR')
+                            {
+                                $query .= ($countFiltrosAux > 1 ? ' AND ' : ' ') . $_array['value'];
+                            }
+                            else
+                            {
+                                $query .= ($countFiltrosAux > 1 ? ' AND ' : ' ') . $_array['field'] . ' ' . $operator . ' ' . $_array['value'] . ' ';
+                            }
+                        }
+                    }
+                }
+
+
 
         } catch (Exception $exception) {
             return $exception;
