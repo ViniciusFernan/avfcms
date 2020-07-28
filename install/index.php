2f32535bad7af8afd3624bc0d0bbd61fc92992c1
ViniciusFernandes <viniciusblink@hotmail.com>
2020-07-28 15:41:33 -0300

    init transational

--- install/index.php
+++ install/index.php
@@ -1,11 +1,27 @@
 <?php
 
+$configCms=null;
+if(!empty($_POST)){
+    $configCms = [
+        'db_host' => $_POST['db_host'],
+        'db_name' => $_POST['db_name'],
+        'db_user' => $_POST['db_user'],
+        'db_password' => $_POST['db_password'],
+        'db_port' => $_POST['db_port'],
+        'nome_projeto' => $_POST['nome_projeto'],
+        'url_projeto' => $_POST['url_projeto'],
+        'user_email' => $_POST['user_email'],
+        'password' => $_POST['password']
+    ];
+}
+
 ?>
 <html >
 <head>
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
 </head>
 <body>
@@ -16,61 +32,94 @@
         <form id="loginform" class="form-horizontal" role="form" action="">
             <div class="mainbox col-md-6 col-sm-8">
             <div class="panel panel-success" >
-                <div class="panel-heading"><div class="panel-title">Configurar Banco de dados</div></div>
+                <div class="panel-heading"><div class="panel-title">Configurar Banco de Dados</div></div>
                 <div style="padding-top:30px" class="panel-body" >
-
+                    <label>Url de acesso ao banco de dados</label>
                     <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <span class="input-group-addon">
+                            <i class="fa fa-link" aria-hidden="true"></i>
+                        </span>
                         <input type="text" class="form-control" name="db_host" placeholder="Url do banco">
                     </div>
 
+                    <label>Nome do banco de dados</label>
                     <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <span class="input-group-addon">
+                            <i class="fa fa-database" aria-hidden="true"></i>
+                        </span>
                         <input type="text" class="form-control" name="db_name" placeholder="Nome do banco">
                     </div>
 
+                    <label>usuario de acesso ao banco de dados</label>
                     <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <span class="input-group-addon">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                        </span>
                         <input type="text" class="form-control" name="db_user" placeholder="Usuario do banco">
                     </div>
 
+                    <label>Senha de acesso ao banco de dados</label>
                     <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <span class="input-group-addon">
+                            <i class="fa fa-unlock" aria-hidden="true"></i>
+                        </span>
                         <input type="text" class="form-control" name="db_password" placeholder="Senha do banco">
                     </div>
 
+                    <label>Porta de conexão ao banco de dados</label>
                     <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-door"></i></span>
+                        <span class="input-group-addon">
+                           <i class="fa fa-cloud" aria-hidden="true"></i>
+                        </span>
                         <input type="text" class="form-control" name="db_port" placeholder="Porta do banco" value="3306">
-                    </div>
-
-                    <div style="margin-top:10px" class="form-group">
-                        <div class="col-sm-12 controls">
-                            <a id="btn-login" href="#" class="btn btn-success">Salvar  </a>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
             <div class="mainbox col-md-6 col-sm-8">
             <div class="panel panel-info" >
-                <div class="panel-heading"><div class="panel-title">Configurar usuario de acesso ao CMS</div></div>
+                <div class="panel-heading"><div class="panel-title">Configurar CMS</div></div>
                 <div style="padding-top:30px" class="panel-body" >
+
+                    <label>Nome do projeto</label>
                     <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username or email">
+                        <span class="input-group-addon">
+                            <i class="fa fa-address-card" aria-hidden="true"></i>
+                        </span>
+                        <input type="text" class="form-control" name="nome_projeto" value="" placeholder="Loja do tio">
                     </div>
 
+                    <label>Url do projeto</label>
                     <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
+                        <span class="input-group-addon">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                        </span>
+                        <input type="text" class="form-control" name="url_projeto" value="" placeholder="lojadotio.com.br">
                     </div>
 
-                    <div style="margin-top:10px" class="form-group">
-                        <div class="col-sm-12 controls">
-                            <a id="btn-login" href="#" class="btn btn-success">Salvar  </a>
-                        </div>
+                    <label>Usuario de acesso administrador (Email)</label>
+                    <div style="margin-bottom: 25px" class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                        </span>
+                        <input type="text" class="form-control" name="user_email" value="" placeholder="meuemail@admin.com.br">
                     </div>
+
+                    <label>Senha de acesso administrador</label>
+                    <div style="margin-bottom: 25px" class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-unlock" aria-hidden="true"></i>
+                        </span>
+                        <input type="password" class="form-control" name="password" placeholder="password">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid p-3 p-md-5">
+            <div style="margin:10px 0 20px" class="col-sm-12 ">
+                <div class="pull-right controls">
+                    <a id="btn-login" href="#" class="btn btn-success">Salvar  </a>
                 </div>
             </div>
         </div>
