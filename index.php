ef2ba19662d401f65bd1c871e254e16ec7e4f48e
ViniciusFernandes <viniciusblink@hotmail.com>
2020-07-28 12:01:53 -0300

    init transational

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 if (file_exists(__DIR__.'/config/config.avf')) :
     define('App', parse_ini_file(__DIR__.'/config/config.avf', true));
 else :
-    echo "O arquivo [config/config.avf] não encontrado";
+    header('Location: ./install/index.php');
 endif;
 
 
