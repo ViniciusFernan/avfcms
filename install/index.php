ac1dca2d7b8af4e71cd4a94eaffcaffb47944f28
ViniciusFernandes <viniciusblink@hotmail.com>
2020-08-01 21:00:06 -0300

    init transational

--- install/index.php
+++ install/index.php
@@ -1,7 +1,7 @@
 <?php
 function curPageURL() {
-    $url =( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
-    $url.=( $_SERVER["SERVER_PORT"] != 80 ) ? ":".$_SERVER["SERVER_PORT"] : "";
+    $url =($_SERVER["HTTPS"] != 'on') ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
+    $url.=($_SERVER["SERVER_PORT"] != 80) ? ":".$_SERVER["SERVER_PORT"] : "";
     $url. $_SERVER["REQUEST_URI"];
     return $url = str_replace(['/install', '/install/'], '', $url);
 }
